cd9b58c1df53bb88117cef65638758ae410eda11
Larry <larry.avery@gmail.com>
2020-01-30 09:51:01 -0600

    Created 51 tests for Cycle learning, none completed yet.

---
+++ src/test/java/com/emerigen/infrastructure/learning/CycleTest.java
@@ -0,0 +1,254 @@
+package com.emerigen.infrastructure.learning;
+
+import static org.assertj.core.api.Assertions.fail;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CycleTest {
+
+	@Test
+	public final void givenDifferentTypes_whenCycleMerged_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNullOrEmptySensorEvents_whenCycleCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveCycleDuration_whenCycleCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveCycleStartTime_whenCycleCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveStandardDeviation_whenCycleCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNullOrEmptySensorEvent_whenCycleNodeCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveOriginStartTime_whenCycleNodeCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenOriginStartTimeNotBasedOnCycleType_whenCycleCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDailyCycle_whenCycleCreated_thenOriginTimeMustBe12am() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDailyCycle_whenCycleCreated_thenDurationMustBe24Hours() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenHourlyCycle_whenCycleCreated_thenOriginTimeMustBe0Minutes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDailyCycle_whenCycleCreated_thenDurationMustBe60Minutes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenWeeklyCycle_whenCycleCreated_thenOriginTimeMustBeSunday12am() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenWeeklyCycle_whenCycleCreated_thenDurationMustBe168Hours() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMonthlyCycle_whenCycleCreated_thenOriginTimeMustBe12amFirstDayOfMonth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMonthlyCycle_whenCycleCreated_thenDurationMustBeApproximately30Days() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenYearlyCycle_whenCycleCreated_thenOriginTimeMustBeJan1_12am() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenYearlyCycle_whenCycleCreated_thenDurationMustBeApproximately365DaysTimes24Hours() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveDataPointDuration_whenCycleNodeCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveStandardDeviation_whenCycleNodeCreated_thenIllegalArgurmentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDifferenceDouble_whenGetStandardDeviation_thenValid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidParameters_whenDifferenceCalculatedBySensor_thenCorrect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDifferencePastEqualityThreshold_whenGetStandardDeviation_thenNotEqual() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenInts_whenGetStandardDeviation_thenValueCorrect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenFloats_whenGetStandardDeviation_thenValueCorrect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenDoubles_whenGetStandardDeviation_thenValueCorrect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNodesWithinStandardDeviationForEquality_whenCheckedForEquality_thenTrue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNodesNotWithinStandardDeviationForEquality_whenCheckedForEquality_thenFalse() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNodeWithValidStartTimeAndOffst_whenRetrieved_thenCorrect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidNodeStartTime_whenSetOnNode_thenTrue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenInvalidNodeStartTime_whenSetOnNode_thenFalse() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenSensorEventListWithDifferentSensorTypes_whenCreatingCycle_thenIllegalArgumentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidCycleNodes_whenDurationsAdded_thenMustEqualCycleDuration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenInvalidStandardDeviation_whenSetOnNode_thenIllegalArgumentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNonPositiveDataPointDuration_whenSetOnNode_thenIllegalArgumentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNullSensorEvent_whenSetOnNode_thenIllegalArgumentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidSensorEvents_whenCycleCreated_thenNodesMustBeInAscendingTimeOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenNullCycleNode_whenMerged_thenIllegalArgumentException() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidCycle_whenCurrentNodeBeforeLastNodeSelected_thenPredictionsMustBeValid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidSortedSensorEvents_whenNodesCreated_thenTimestampsAndOffsetsValid() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenValidSortedSensorEvents_whenNodesCreated_thenTimestampsAndOffsetsInAscendingOrder() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMultipleValidCyclesForSensor_whenCurrentNodeBeforeLastNodeSelectedOnAnyCycle_thenPredictionsOnAllCyclesMustBeReturned() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMultipleCyclesForSensor_whenCurrentSensorEventMatches_thenCorrectCycleNodesArePredicted() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMultipleCyclesForSensor_whenCyclesMerged_thenCyclesMergedAtJoinedPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void givenMultipleCyclesForSensor_whenCyclesMerged_thenCyclesBranchedAtDistinctPoints() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
