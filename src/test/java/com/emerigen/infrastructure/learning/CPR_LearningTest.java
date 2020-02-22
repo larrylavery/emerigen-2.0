482757230f4490c56775b0aca4a084cbcd4bf370
Larry <larry.avery@gmail.com>
2020-02-22 10:00:17 -0600

    CPR_LearningTest done except for 2 near future implementations

--- src/test/java/com/emerigen/infrastructure/learning/CPR_LearningTest.java
+++ src/test/java/com/emerigen/infrastructure/learning/CPR_LearningTest.java
@@ -3,21 +3,35 @@ package com.emerigen.infrastructure.learning;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
-import java.util.Random;
 import java.util.UUID;
 
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import com.emerigen.infrastructure.repository.KnowledgeRepository;
 import com.emerigen.infrastructure.sensor.Sensor;
-import com.emerigen.infrastructure.sensor.SensorEvent;
 import com.emerigen.infrastructure.sensor.SensorManager;
+import com.emerigen.infrastructure.utils.EmerigenProperties;
 
 public class CPR_LearningTest {
+
+	private long minimumDelayBetweenReadings = Long
+			.parseLong(EmerigenProperties.getInstance()
+					.getValue("sensor.default.minimum.delay.between.readings.millis"))
+			* 1000000;
+
+	@Mock
+	PredictionService mockPredictionService;
+
+	@Rule
+	public MockitoRule mockitoRule = MockitoJUnit.rule();
 
 	public static Cycle createCycle(String cycleType, int sensorType, int sensorLocation,
 			int numberOfNodes) {
@@ -35,45 +49,6 @@ public class CPR_LearningTest {
 			throw new IllegalArgumentException(
 					"cycle type must be valid, but was (" + cycleType + ")");
 
-		// Set attributes
-//		cycle.setPreviousCycleNodeIndex(0);
-//		CycleNode cycleNode;
-//
-//		for (int i = 0; i < numberOfNodes; i++) {
-//			cycleNode = new CycleNode();
-//			int minimumDelayBetweenReadings;
-//			int reportingMode;
-//			boolean wakeUpSensor;
-//			SensorEvent sensorEvent = new SensorEvent();
-//			sensorEvent.setTimestamp(i * sensorEvent.getTimestamp());
-//			Sensor sensor;
-//
-//			// Create SensorEvent
-//			sensorEvent.setSensorType(sensorType);
-//			sensorEvent.setSensorLocation(sensorLocation);
-//
-//			// Set sensor event values
-//			float[] values = { 1.0f + (i + 1) * 100.0f, 2.0f + (i + 1) * 100.0f };
-//			sensorEvent.setValues(values);
-//
-//			// create and set event sensor
-//			minimumDelayBetweenReadings = Sensor.DELAY_NORMAL;
-//			reportingMode = Sensor.REPORTING_MODE_ON_CHANGE;
-//			wakeUpSensor = false;
-//			sensor = SensorManager.getInstance().getDefaultSensorForLocation(sensorType, sensorLocation);
-//			sensor.setMinimumDelayBetweenReadings(minimumDelayBetweenReadings);
-//			sensor.setWakeUpSensor(wakeUpSensor);
-//			sensor.setReportingMode(reportingMode);
-//			sensorEvent.setSensor(sensor);
-//
-//			// Set up the rest of the CycleNode fields
-//			cycleNode.setSensorEvent(sensorEvent);
-//			cycleNode.setStartTimeOffsetNano(100 * (i + 1));
-//			cycleNode.setDataPointDurationNano(1000 * (i + 1));
-//			cycleNode.setProbability(0.3);
-//			cycleNode.setMyCycle(cycle);
-//			cycle.addCycleNode(cycleNode);
-//		}
 		return cycle;
 	}
 
@@ -95,33 +70,6 @@ public class CPR_LearningTest {
 
 		assertThat(sm.listenerIsRegisteredToSensor(cpr, gpsSensor)).isTrue();
 		// CouchbaseRepository.getInstance().remove("cycle", key);
-	}
-
-	@Test
-	public final void givenValidCycleNodes_whenDurationsAdded_thenMustBeLessThanCycleDuration() {
-		fail("Not yet implemented"); // TODO
-	}
-
-	@Test
-	public final void givenNonEmptyCycle_whenSignificantlyDifferentEventDataPointArrives_thenNewNodeAddedToEndOfCycle() {
-		Sensor gpsSensor = SensorManager.getInstance()
-				.getDefaultSensorForLocation(Sensor.TYPE_GPS, Sensor.LOCATION_PHONE);
-		Cycle gpsCycle = new DailyCycle(Sensor.TYPE_GPS, Sensor.LOCATION_PHONE);
-		CyclePatternRecognizer cpr = new CyclePatternRecognizer(gpsCycle, gpsSensor,
-				new PredictionService());
-
-		// When
-		// gps sensors require lat and long floats
-		Random rd = new Random();
-		float[] values = { rd.nextFloat(), rd.nextFloat() };
-		float[] values2 = { rd.nextFloat() + 10, rd.nextFloat() + 10 };
-		SensorEvent event1 = new SensorEvent(gpsSensor, values);
-		SensorEvent event2 = new SensorEvent(gpsSensor, values2);
-		cpr.onSensorChanged(event1);
-		cpr.onSensorChanged(event2);
-		fail("rewrite");
-		// Then
-		// assertThat(gpsCycle.getNodeList().size()).isEqualTo(2);
 	}
 
 	@BeforeClass
